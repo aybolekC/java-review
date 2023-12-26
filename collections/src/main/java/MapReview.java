44038459733df027e725501d45b4d767466f46a8
Aybolek Hudayberdiyeva <ahudayberdiyeva@signifyhelth.com>
2023-12-26 14:11:11 -0600

    two sum brute force solution

--- collections/src/main/java/MapReview.java
+++ collections/src/main/java/MapReview.java
@@ -12,15 +12,17 @@ public class MapReview {
 
         System.out.println(map.get(2));
 
-        System.out.println(findFirstNonRepeating("ello world java developer"));
+        System.out.println(findFirstNonRepeating("deo ava deveoe"));
 
     }
 
     public static Character findFirstNonRepeating(String str) {
 
         Map<Character, Integer> map = new HashMap<>();
+        //for counting the chars
         int count;
 
+//        counting chars and putting values in the hash map
         for (Character ch : str.toCharArray()) {
             if (map.containsKey(ch)) {
                 count = map.get(ch);
@@ -29,11 +31,13 @@ public class MapReview {
             } else map.put(ch, 1);
         }
 
+        //starting from string, check if the char count equals to 1
         for (Character ch:str.toCharArray()){
             if(map.get(ch)==1) return ch;
 
         }
-        return Character.MIN_VALUE;
+       // return Character.MIN_VALUE; we can return min value or null as well
+        return null;
     }
 
 }
---
+++ collections/src/main/java/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class TwoSum {
+
+
+    public static void main(String[] args) {
+
+        int[] myArray={2,7,9,11};
+        System.out.println(Arrays.toString(twoSum1(myArray, 9)));
+    }
+
+
+    public static int[] twoSum1(int[] array, int target){
+
+        for (int i=0; i<array.length-1;i++){  //n
+            for (int j=i+1; j<array.length; j++){  //n
+                if(array[i]+array[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    //n*n
+
+
+}
